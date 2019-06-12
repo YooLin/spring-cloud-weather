d5d6a4392b6c1dd87f93a81513a9ef612fd52acf
linyicong <401664157@qq.com>
2019-06-13 00:02:58 +0800

    hystrix+turbine => spring cloud alibaba sentinel

--- weather-city-microservice/src/main/groovy/com/imooc/spring/cloud/weather/city/WeatherCityApplication.groovy
+++ weather-city-microservice/src/main/groovy/com/imooc/spring/cloud/weather/city/WeatherCityApplication.groovy
@@ -2,13 +2,11 @@ package com.imooc.spring.cloud.weather.city
 
 import org.springframework.boot.SpringApplication
 import org.springframework.cloud.client.SpringCloudApplication
-import org.springframework.cloud.netflix.hystrix.EnableHystrix
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 /**
  * @author yicong* @since 2019-05-22 11:57
  */
-@EnableHystrix
 @EnableFeignClients
 @SpringCloudApplication
 class WeatherCityApplication {
